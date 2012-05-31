6b7626b87d71401a5af316cf7f87eeedcc337e8a
Yuqing Zhang <zhangyuqing524@gmail.com>
2012-05-31 14:44:18 +0200

    Use base.js for DOM operation

---
+++ static/scripts/base.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+	$('#foot-text').append('&copy; ' + new Date().getFullYear());
+	
+	$("img.pic-next-text")
+		.mouseover(function() { 
+			$(this).attr("src", "static/imgs/icons/metroicons/back_hover.png");
+		})
+		.mouseout(function() {
+			$(this).attr("src", "static/imgs/icons/metroicons/back.png");
+		});
+});
