28265acd82e28d683613ada0041c2972ab4ebf78
YUQING ZHANG <zhangyuqing524@YUQING-ZHANGs-MacBook-Pro.local>
2012-06-18 22:38:54 +0200

    More tests

--- tools/juicebox.py
+++ tools/juicebox.py
@@ -1,21 +1,28 @@
 from SitesToAzureBlob import SitesToAzureBlob
-from xml.dom.minidom import parse, parseString
-import os, filecmp
+from xml.dom.minidom import parse
+import os
 
-def upload_images():
-    stb = SitesToAzureBlob('../src/juicebox/gallery/', 
+def upload_images(account_name, account_key):
+    stb = SitesToAzureBlob('../gallery/', 
                            '../output/gallery/', overwrite_output = False,
-                           account_name = 'zhangyuqinglabs', account_key = 'aQLXzo4soX+dK1TYqTeG9dxkdgGpDi8dEbODL8uha5P3bG1WvuczRJDpopO1JKLba6Y9AqX0yEwq/s8Zbo+60Q==',
+                           account_name = account_name, account_key = account_key,
                            container_name = 'gallery')
 
     stb.upload_files_to_blob()
 
+def update_backgroundColor(index_page_path):
+    content = open(os.path.abspath(index_page_path), 'r').read()
+    content = content.replace('backgroundColor: \'#222222\'', 'backgroundColor: \'white\'')
+    f = open(os.path.abspath(index_page_path), 'w')
+    f.write(content)
+    f.close()
+    
 def update_config_xml(config_xml_path, gallery_name):
-    image_container = 'http://zhangyuqinglabs.blob.core.windows.net/garllery/'
+    image_container = 'http://zhangyuqinglabs.blob.core.windows.net/gallery/'
     dom = parse(os.path.abspath(config_xml_path))
     images = dom.getElementsByTagName('image')
     for image in images:
-        if not image.image.attributes['linkURL'].value.startswith(image_container):
+        if not image.attributes['linkURL'].value.startswith(image_container):
             image.attributes['linkURL'].value = (image_container + gallery_name + '/' + image.attributes['linkURL'].value).lower()
             image.attributes['thumbURL'].value = (image_container+ gallery_name + '/' + image.attributes['thumbURL'].value).lower()
             image.attributes['imageURL'].value = (image_container + gallery_name + '/' + image.attributes['imageURL'].value).lower()
--- tools/yuqingzhang.py
+++ tools/yuqingzhang.py
@@ -1,15 +1,18 @@
-from juicebox import upload_images, update_config_xml
+from juicebox import upload_images, update_config_xml, update_backgroundColor
 from pelicantool import call_pelican
 from SitesToAzureBlob import SitesToAzureBlob
 
-def main():
+def main(account_name = None, account_key = None, ):
     #update_config_xml(r'..\src\main\gallery\2011-09-edinburgh-scotland\config.xml', '2011-09-edinburgh-scotland')
     #update_config_xml(r'..\src\main\gallery\2011-09-highlands-scotland\config.xml', '2011-09-highlands-scotland')
     #update_config_xml(r'..\src\main\gallery\2012-05-stavanger-norway\config.xml', '2012-05-stavanger-norway')
-
-    #upload_images()
+    #update_config_xml(r'../src/main/gallery/2011-09-london-england/config.xml', '2011-09-london-england')
     
+    #update_backgroundColor(r'../src/main/gallery/2011-09-london-england/2011-09-london-england.html')
     print 'Juciebox Processed...'
+    
+    upload_images(account_name, account_key)
+    
     try:
         call_pelican('Pelican/settings.py', '../src/Pelican') 
     except Exception, e:
@@ -27,7 +30,8 @@ def main():
     
     print 'Pelican Processed...'
 
-    stb = SitesToAzureBlob('../src/main', '../output', overwrite_output = False)
+    stb = SitesToAzureBlob('../src/main', '../output', overwrite_output=False, account_name = account_name, account_key = account_key,
+                         container_name = 'site')
     stb.upload_files_to_blob()
 
 if __name__ == "__main__":
