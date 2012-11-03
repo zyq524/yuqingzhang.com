200dfb70e55256f4e046802569094b74c34904b0
Yuqing Zhang <zhangyuqing524@gmail.com>
2012-11-03 22:38:35 +0100

    Added a new post

--- tools/yuqingzhang.py
+++ tools/yuqingzhang.py
@@ -7,7 +7,7 @@ def main(account_name = None, account_key = None, ):
     #update_config_xml(r'..\src\main\gallery\2011-09-highlands-scotland\config.xml', '2011-09-highlands-scotland')
     #update_config_xml(r'..\src\main\gallery\2012-05-stavanger-norway\config.xml', '2012-05-stavanger-norway')
     #update_config_xml(r'../src/main/gallery/2011-09-london-england/config.xml', '2011-09-london-england')
-    
+    #update_config_xml(r'D:\Users\qizhang\Desktop\Reberbanegade\config.xml',r'http://zhangyuqinglabs.blob.core.windows.net/others/', r'Reberbanegade');
 #    create_index_page('../src/main/footprints', '2011-09-edinburgh-scotland', 'Edinburgh, Scotland')
 #    create_index_page('../src/main/footprints', '2011-09-highlands-scotland', 'Highlands, Scotland')
 #    create_index_page('../src/main/footprints', '2011-09-london-england', 'London, England')
@@ -15,24 +15,24 @@ def main(account_name = None, account_key = None, ):
     
     print 'Juciebox Processed...'
     
-    upload_images(account_name, account_key)
+    #upload_images(account_name, account_key)
     
-    try:
-        call_pelican('Pelican/settings.py', '../src/Pelican') 
-    except Exception, e:
-        if e[0] ==5 and e[1] == 'Access is denied':
-            try:
-                call_pelican('Pelican/settings.py', '../src/Pelican') 
+    #try:
+    #    call_pelican('Pelican/settings.py', '../src/Pelican') 
+    #except Exception, e:
+    #    if e[0] ==5 and e[1] == 'Access is denied':
+    #        try:
+    #            call_pelican('Pelican/settings.py', '../src/Pelican') 
 
-            except Exception, e:
-                if e[0] ==5 and e[1] == 'Access is denied':
-                    try:
-                        call_pelican('Pelican/settings.py', '../src/Pelican') 
+    #        except Exception, e:
+    #            if e[0] ==5 and e[1] == 'Access is denied':
+    #                try:
+    #                    call_pelican('Pelican/settings.py', '../src/Pelican') 
 
-                    except Exception, e:
-                        raise
-    
-    print 'Pelican Processed...'
+    #                except Exception, e:
+    #                    raise
+    #
+    #print 'Pelican Processed...'
 
     stb = SitesToAzureBlob('../src/main', '../output', overwrite_output=False, account_name = account_name, account_key = account_key,
                          container_name = 'site')
